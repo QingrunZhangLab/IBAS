2ab20b7802e89e68be134cdef83d57c46d735acf
Pathum Kossinna <thalagalakossinnagep@ucalgary.ca>
2022-08-25 03:16:23 -0400

    Added PathwayReduction and snpAvailability functions

--- SampleScript.R
+++ SampleScript.R
@@ -1,3 +1,48 @@
 source("inTWAS_Functions.R")
 
-pathways <- pathwayLoader(forceDownload = TRUE)
+pathways <- pathwayLoader(forceDownload = FALSE)
+pathways$MedianEdges <- ceiling(pathways$MedianEdges)
+
+expr.data <- fread("../../Data/GTEx/GTEx_Analysis_v8_eQTL_expression_matrices/Whole_Blood.v8.normalized_expression.bed")
+expr.data$start <- NULL
+expr.data$end <- NULL
+expr.data$`#chr` <- NULL
+expr.data <- transpose(expr.data, keep.names = "SampleID", make.names = "gene_id")
+sample.ids <- expr.data$SampleID
+expr.data$SampleID <- NULL
+
+gencode <- as.data.table(str_split(fread("../../Data/GTEx/gencode.v26.GRCh38.genes.gtf")$V9, ";", simplify = TRUE)[, c(1,4)])
+colnames(gencode) <- c("geneID", "geneName")
+gencode$geneID <- str_remove(gencode$geneID, "gene_id ")
+gencode$geneName <- str_remove(gencode$geneName, "gene_name ")
+gencode$geneID <- gsub('[\"]', '', gencode$geneID)
+gencode$geneName <- gsub('[\"]', '', gencode$geneName)
+gencode$geneName <- trimws(gencode$geneName, which = "both")
+gencode <- unique(gencode)
+
+pathwayReducer(exprData = expr.data, sampleIDs = sample.ids, pathwayData = pathways, 
+               geneLookupTable = gencode, method = "tSNE", numDims = 2, 
+               outputFolder = "./ReducedPathways", seed = 1234)
+
+pathwayReducer(exprData = expr.data, sampleIDs = sample.ids, pathwayData = pathways, 
+               geneLookupTable = gencode, method = "UMAP", numDims = 2, 
+               outputFolder = "./ReducedPathways", seed = 1234)
+
+gencode <- fread("../../Data/GTEx/gencode.v26.GRCh38.genes.gtf")
+mappings <- as.data.table(str_split(gencode$V9, ";", simplify = TRUE)[, c(1, 4)])
+colnames(mappings) <- c("gene_id", "gene_name")
+mappings <- cbind(gencode, mappings)
+mappings$gene_id <- str_replace(mappings$gene_id, "gene_id ", "")
+mappings$gene_id <- str_replace_all(mappings$gene_id, '[\"]', "")
+mappings$gene_name <- str_replace(mappings$gene_name, "gene_name ", "")
+mappings$gene_name <- str_replace_all(mappings$gene_name, '[\"]', "")
+mappings$gene_name <- str_trim(mappings$gene_name)
+
+mappings <- mappings[mappings$V3 == "gene", ]
+mappings <- mappings[mappings$gene_name %in% setdiff(unique(mappings$gene_name), unique(mappings$gene_name[duplicated(mappings$gene_name)]))]
+mappings <- mappings[, c(1, 4, 5, 11)]
+colnames(mappings) <- c("chromosome", "start", "end", "gene_name")
+mappings$chromosome <- str_remove_all(mappings$chromosome, "chr")
+mappings <- mappings[!grepl("\\D", mappings$chromosome), ]
+
+snpAvailability(gencode = mappings, pathwayData = pathways, cisBP = 1000000, outputFolder = "./SNPAvailability")
--- inTWAS_Functions.R
+++ inTWAS_Functions.R
@@ -4,7 +4,9 @@ pacman::p_load(data.table,
                stringr,
                optparse,
                KEGGREST,
-               KEGGgraph)
+               KEGGgraph,
+               Rtsne,
+               umap)
 
 downloadPathways <- function(downloadLocation = "./PathwayData.csv"){
   
@@ -20,7 +22,8 @@ downloadPathways <- function(downloadLocation = "./PathwayData.csv"){
     
     print(paste0("Working on ", query[[1]]$NAME, ". Pathway ", i, " of ", length(uniquePathways), "..."))
     
-    tmpFile <- tempfile(fileext = ".xml")
+    tmpFile <- "./tmpfile.xml"
+    file.create(tmpFile)
     tryCatch(retrieveKGML(uniquePathways[i], organism = "hsa", destfile = tmpFile, method = "libcurl", quiet = TRUE), error = function(e) print("Error?"))
     mapkG <- parseKGML2Graph(tmpFile, expandGenes = TRUE, genesOnly = TRUE)
     medianEdges <- median(lengths(edges(mapkG)))
@@ -58,8 +61,87 @@ pathwayLoader <- function(cacheLocation = "./PathwayData.csv", forceDownload = F
   
 }
 
-pathwayReducer <- function(exprData, method = "tSNE", numDims = 2, pathwayData){
+# exprData <- expr.data
+# sampleIDs <- sample.ids
+# method = "tSNE"
+# numDims <- 2
+# pathwayData <- pathways
+# geneLookupTable <- gencode
+# outputFolder = "./ReducedPathways"
+# seed = 2222
+pathwayReducer <- function(exprData, sampleIDs, pathwayData, 
+                           geneLookupTable = NULL, method = "tSNE", numDims = 2,
+                           outputFolder = "./ReducedPathways",
+                           seed = 2222){
   
+  pathwayData <- pathwayData[MedianEdges >= 3]
   
+  if(!dir.exists(outputFolder)) dir.create(outputFolder, recursive = TRUE)
+  
+  for(i in seq(1, nrow(pathwayData))){
+    pathwayName <- pathwayData$PathwayID[i]
+    pathwayGenes <- str_split(pathwayData$All_Genes[i], ";")[[1]]
+    
+    if(!is.null(geneLookupTable)){
+      pathwayGenes <- geneLookupTable[[1]][geneLookupTable[[2]] %in% pathwayGenes]
+    }
+    
+    genesToSelect <- intersect(pathwayGenes, colnames(exprData))
+    
+    if(length(genesToSelect)<1){
+      print(paste0("Insufficient genes for pathway ", pathwayName, ". Skipping reduction."))
+      next
+    }
+    
+    pathwayExprData <- as.matrix(exprData[, ..genesToSelect])
+    
+    if(tolower(method) == "tsne"){
+      set.seed(seed)
+      pathwayReduced <- Rtsne(pathwayExprData, dims = numDims, perplexity = pathwayData$MedianEdges[i])$Y
+    }else if(tolower(method) == "umap"){
+      pathwayReduced <- umap(pathwayExprData, n_neighbors = pathwayData$MedianEdges[i], 
+                             n_components = numDims, random_state = seed)$layout
+    }else{
+      stop("No valid method specified!")
+    }
+    
+    pathwayReduced <- data.table(ID = sampleIDs, pathwayReduced)
+    fwrite(pathwayReduced, paste0(outputFolder, "/", method, "_", pathwayName, ".tsv"), sep = "\t")
+    
+  }
+  
+}
+
+# gencode <- mappings
+# pathwayData <- pathways
+# cisBP <- 1000000
+# outputFolder = "./SNPAvailability"
+snpAvailability <- function(gencode, pathwayData, cisBP = 1000000, 
+                            outputFolder = "./SNPAvailability"){
+  
+  if(!dir.exists(outputFolder)) dir.create(outputFolder, recursive = TRUE)
+  
+  for(i in 1:nrow(pathwayData)){
+    
+    print(paste0("Currently working on ", pathwayData$PathwayID[i], " (", i, " of ", nrow(pathwayData), ")..."))
+    if(file.exists(paste0(outputFolder, "/", pathwayData$PathwayID[i], ".tsv"))){
+      print(paste0("Pathway ", pathwayData$PathwayID[i], " SNP file exists. Skipping..."))
+      next
+    }
+    
+    curr_genes <- str_split(pathwayData$All_Genes[i], ";")[[1]]
+    
+    gene_details <- gencode[gencode$gene_name %in% curr_genes,]
+    gene_details$start <- pmax(gene_details$start - cisBP, 0)
+    gene_details$end <- gene_details$end + cisBP
+    
+    region_file <- gene_details[, 1:3]
+    region_file$POS <- paste0(region_file$start, ":", region_file$end)
+    region_file <- region_file[, c(1,4)]
+    colnames(region_file) <- c("CHR", "LOC")
+    
+    print(paste0("Writing pathway ", pathwayData$PathwayID[i]))
+    fwrite(region_file, paste0(outputFolder, "/", pathwayData$PathwayID[i], ".tsv"), sep = "\t")
+  }
   
 }
