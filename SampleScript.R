ae44758bc1ff54b7dbc410433c7b44077ca82b96
Pathum Kossinna <55003959+patrickdeeskay@users.noreply.github.com>
2022-08-29 21:57:15 -0400

    Update SampleScript.R

--- SampleScript.R
+++ SampleScript.R
@@ -1,10 +1,10 @@
-source("inTWAS_Functions.R")
+source("IBAS_Functions.R")
 
 pathways <- pathwayLoader(forceDownload = FALSE)
 pathways$MedianEdges <- ceiling(pathways$MedianEdges)
 
 # expr.data <- fread("../../Data/GTEx/GTEx_Analysis_v8_eQTL_expression_matrices/Whole_Blood.v8.normalized_expression.bed")
-expr.data <- fread("../../Data/GTEx/Whole_Blood.v8.residuals.csv")
+expr.data <- fread("/Data/GTEx/Whole_Blood.v8.residuals.csv")
 expr.data$start <- NULL
 expr.data$end <- NULL
 expr.data$`#chr` <- NULL
@@ -12,7 +12,7 @@ expr.data <- transpose(expr.data, keep.names = "SampleID", make.names = "gene_id
 sample.ids <- expr.data$SampleID
 expr.data$SampleID <- NULL
 
-gencode <- as.data.table(str_split(fread("../../Data/GTEx/gencode.v26.GRCh38.genes.gtf")$V9, ";", simplify = TRUE)[, c(1,4)])
+gencode <- as.data.table(str_split(fread("/Data/GTEx/gencode.v26.GRCh38.genes.gtf")$V9, ";", simplify = TRUE)[, c(1,4)])
 colnames(gencode) <- c("geneID", "geneName")
 gencode$geneID <- str_remove(gencode$geneID, "gene_id ")
 gencode$geneName <- str_remove(gencode$geneName, "gene_name ")
